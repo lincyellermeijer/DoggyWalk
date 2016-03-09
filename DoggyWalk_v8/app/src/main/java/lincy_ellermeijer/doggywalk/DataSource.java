a11fd106cbf4683c9d5b80c704e6861e7fca2ce6
Lincy Ellermeijer <lincy_95@hotmail.com>
2016-03-09 23:50:31 +0100

    Doggy Walk version 8 - working improved database, still need to add 2nd table for Routes

---
+++ DoggyWalk_v8/app/src/main/java/lincy_ellermeijer/doggywalk/DataSource.java
@@ -0,0 +1,153 @@
+package lincy_ellermeijer.doggywalk;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.CursorIndexOutOfBoundsException;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lincypc on 7-3-2016.
+ */
+public class DataSource {
+
+    private SQLiteDatabase database;
+    private MySQLiteHelper dbHelper;
+    //private String[] plantAllColumns = { MySQLiteHelper.COLUMN_PLANT_ID, MySQLiteHelper.COLUMN_PLANT_TITLE, MySQLiteHelper.COLUMN_PLANT_DESCRIPTION };
+    //private String[] routeAllColumns = { MySQLiteHelper.COLUMN_ROUTE_ID, MySQLiteHelper.COLUMN_ROUTE };
+
+
+    public DataSource(Context context) {
+        dbHelper = new MySQLiteHelper(context);
+        database = dbHelper.getWritableDatabase();
+        dbHelper.close();
+    }
+
+    // Opens the database to use it
+    public void open() throws SQLException {
+        database = dbHelper.getWritableDatabase();
+    }
+
+    // Closes the database when you no longer need it
+    public void close() {
+        dbHelper.close();
+    }
+
+    // CREATE
+    // adding plants
+    public long addPlant (Plant plant) {
+        // If the database is not open yet, open it
+        if (!database.isOpen())
+            open();
+
+        // putting the values of the title, description and image
+        ContentValues values = new ContentValues();
+        values.put(dbHelper.COLUMN_PLANT_TITLE, plant._plantTitle);
+        values.put(dbHelper.COLUMN_PLANT_DESCRIPTION, plant._plantDescription);
+        values.put(dbHelper.COLUMN_PLANT_IMAGE, plant._plantImage);
+
+        // inserting values into table plants
+        long insertId = database.insert(dbHelper.TABLE_PLANTS, null, values);
+
+        // If the database is open, close it
+        if (database.isOpen())
+            close();
+        return insertId;
+    }
+
+    // READ
+    // Getting a single plant
+    Plant getPlant(long id) {
+        database = dbHelper.getReadableDatabase();
+
+        Cursor cursor = database.query(dbHelper.TABLE_PLANTS, new String[]{dbHelper.COLUMN_PLANT_ID, dbHelper.COLUMN_PLANT_TITLE, dbHelper.COLUMN_PLANT_DESCRIPTION, dbHelper.COLUMN_PLANT_IMAGE}, dbHelper.COLUMN_PLANT_ID + "=?",
+                new String[]{String.valueOf(id)}, null, null, null, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Plant plant = new Plant(Integer.parseInt(cursor.getString(0)),
+                cursor.getString(1), cursor.getString(1), cursor.getBlob(1));
+
+        // return plant
+        return plant;
+
+    }
+
+    // Getting all plants
+    public List<Plant> getAllPlants() {
+        if (!database.isOpen())
+            open();
+
+        List<Plant> plantList = new ArrayList<Plant>();
+        // Select all query
+        String selectQuery = "SELECT * FROM plants ORDER BY plant_title";
+
+        Cursor cursor = database.rawQuery(selectQuery, null);
+        //Looping through all rows and adding to list
+
+        if (cursor.moveToFirst()) {
+            do {
+                Plant plant = new Plant();
+                plant.setID(Integer.parseInt(cursor.getString(0)));
+                plant.setPlantTitle(cursor.getString(1));
+                plant.setPlantDescription(cursor.getString(2));
+                plant.setPlantImage(cursor.getBlob(3));
+                // Adding plant to list
+                plantList.add(plant);
+            } while (cursor.moveToNext());
+        }
+        // If the database is open, close it
+        if (database.isOpen())
+            close();
+        // return plant list
+        return plantList;
+
+    }
+
+    // UPDATE
+    public int updatePlant(Plant plant) {
+        if (!database.isOpen())
+            open();
+
+        // putting the values of the title, description and image
+        ContentValues values = new ContentValues();
+        values.put(dbHelper.COLUMN_PLANT_TITLE, plant.getPlantTitle());
+        values.put(dbHelper.COLUMN_PLANT_DESCRIPTION, plant.getPlantDescription());
+        values.put(dbHelper.COLUMN_PLANT_IMAGE, plant.getPlantImage());
+
+        // updating the database
+        return database.update(dbHelper.TABLE_PLANTS, values, dbHelper.COLUMN_PLANT_ID + " = ?",
+                new String[] { String.valueOf(plant.getID()) });
+
+    }
+
+     // DELETE
+    public void deletePlant(Plant plant) {
+        if (!database.isOpen())
+            open();
+
+        database.delete(dbHelper.TABLE_PLANTS, dbHelper.COLUMN_PLANT_ID + " = ?",
+                new String[] { String.valueOf(plant.getID()) });
+
+        if (database.isOpen())
+            close();
+    }
+
+    // Getting plants Count
+    public int getContactsCount() {
+        String countQuery = "SELECT * FROM " + dbHelper.TABLE_PLANTS;
+        dbHelper.getReadableDatabase();
+        Cursor cursor = database.rawQuery(countQuery, null);
+        cursor.close();
+
+// return count
+        return cursor.getCount();
+    }
+
+
+
+}
---
+++ DoggyWalk_v8/app/src/main/java/lincy_ellermeijer/doggywalk/PlantDetailsActivity.java
@@ -0,0 +1,131 @@
+package lincy_ellermeijer.doggywalk;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * Created by lincypc on 15-2-2016.
+ */
+public class PlantDetailsActivity extends AppCompatActivity {
+
+    private ImageView icon;
+    private TextView title, description;
+
+    private DataSource datasource;
+    private Plant plant;
+    private EditText editTitleText;
+    private EditText editDescriptionText;
+    private Button updateButton;
+
+    final String LOGTAG = "Test debug ";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+       /* //Initialize layout items
+        ImageView image = (ImageView) findViewById(R.id.activity_details_icon);
+        plantTitle = (TextView) findViewById(R.id.activity_details_title);
+        plantDescription = (TextView) findViewById(R.id.activity_details_description);
+
+        //Get the values from the intent
+        int imageResource = getIntent().getIntExtra("image-resource", 0);
+        String titleString = getIntent().getStringExtra("title");
+        String descriptionString = getIntent().getStringExtra("description");
+
+        //Set the values from the intent to the views
+        icon.setImageResource(imageResource);
+        title.setText(titleString);
+        description.setText(descriptionString);
+*/
+        datasource = new DataSource(this);
+        int plantId = getIntent().getIntExtra(PlantListActivity.EXTRA_PLANT_ID, -1);
+        if (plantId != -1) {
+            plant = datasource.getPlant(plantId);
+
+            TextView title = (TextView) findViewById(R.id.activity_details_title);
+            title.setText(plant.getPlantTitle().toString());
+
+            TextView description = (TextView) findViewById(R.id.activity_details_description);
+            description.setText(plant.getPlantDescription().toString());
+
+        ImageView image = (ImageView) findViewById(R.id.activity_details_icon);
+        image.setImageResource(R.drawable.plant_icon);
+
+
+            editTitleText = (EditText) findViewById(R.id.details_updateTitleText);
+            editDescriptionText = (EditText) findViewById(R.id.details_updateDescriptionText);
+
+            updateButton = (Button) findViewById(R.id.details_updateButton);
+
+            updateButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i(LOGTAG, "button clicked");
+                    // get image from drawable
+                    Bitmap image = BitmapFactory.decodeResource(getResources(),
+                            R.drawable.plant_icon);
+
+                    // convert bitmap to byte
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    image.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                    byte imageInByte[] = stream.toByteArray();
+
+                    // Check if the title and descriptions have text
+                    if (!TextUtils.isEmpty(editTitleText.getText()) && !TextUtils.isEmpty(editDescriptionText.getText())) {
+
+                        //long plantId = datasource.addPlant(new Plant(editTitleText.getText().toString(), editDescriptionText.getText().toString(), imageInByte));
+                        plant.setPlantTitle(editTitleText.getText().toString());
+                        plant.setPlantDescription(editDescriptionText.getText().toString());
+                        plant.setPlantImage(imageInByte);
+                        datasource.updatePlant(plant);
+                        Toast.makeText(PlantDetailsActivity.this, "Assignment Updated", Toast.LENGTH_SHORT).show();
+
+                        Intent resultIntent = new Intent();
+                        //resultIntent.putExtra(PlantListActivity.EXTRA_PLANT_ID, plantId);
+                        setResult(Activity.RESULT_OK, resultIntent);
+                        finish();
+
+                    } else {
+                        //Show a message to the user
+                        Toast.makeText(PlantDetailsActivity.this, "Please enter some text in the title and description fields", Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            });
+
+        }
+    }
+
+    // Go to previous screen when app icon in action bar is clicked
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
